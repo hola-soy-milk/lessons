f39cdb6035c4fe6ffe1e396374b3446af97c5a0c
Ramón Huidobro <hola@ramonh.dev>
2022-06-16 21:34:25 +0200

    Add things

--- prototype-class/index.js
+++ prototype-class/index.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+function Pastry(name, hasJam, hasSprinkles) {
+    this.name = name;
+    this.hasJam = hasJam;
+    this.hasSprinkles = hasSprinkles
+}
+
+const cupcake = new Pastry("Cupcake", true, true)
+const muffin = new Pastry("Muffin", true, false)
+
+console.log(cupcake)
+
+Pastry.prototype.servings = 1
+
+console.log(cupcake.hasOwnProperty('servings'))
+console.log(cupcake.__proto__.hasOwnProperty('servings'))
